9cc961b072716392fb661238165d06fcf2407cdf
Letícia Priotto <leticia.priotto28@gmail.com>
2021-10-27 18:20:10 -0300

    criando Migration e Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,33 +12,16 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var string[]
-     */
+    protected $table = 'users';
     protected $fillable = [
         'name',
+        'cpf',
+        'birth_date',
         'email',
-        'password',
+        'phone_number',
+        'address',
+        'city',
+        'state',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('cpf')->unique();
+            $table->date('birth_date');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('phone_number');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
             $table->timestamps();
         });
     }
